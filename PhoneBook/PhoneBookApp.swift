6dd3e712c7a3cd120c84005da29a4fb89acaa758
Nikita Ryzhkin <timongo@bk.ru>
2022-02-03 19:56:25 +0400

    Initial Commit

---
+++ PhoneBook/PhoneBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneBookApp.swift
+//  PhoneBook
+//
+//  Created by Никита Рыжкин on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhoneBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
